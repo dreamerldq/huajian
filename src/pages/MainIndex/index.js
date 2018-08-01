b2af62e518632e870f80ceef6ef618fd8d92d52a
qiuqiu <qiuqiu@xintiaotime.com>
2018-08-01 14:10:42 +0800

    feat: 添加antd mobile && 配置eslint

--- src/pages/MainIndex/index.js
+++ src/pages/MainIndex/index.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// class MainIndex extends React.Component{
-//     constructor(props){
-//         super(props)
-//     }
-//     render(){
-//         return(
-//             <h1>这是首页</h1>
-//         )
-//     }
-// }
-const MainIndex = () => {
-    return(
-        <h1>Hello World</h1>
-    )
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Flex, WingBlank } from 'antd-mobile';
+import styles from './index.less';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  showName() {
+    console.log('LIDANQIU');
+  }
+
+  render() {
+    return (
+            <React.Fragment>
+                <WingBlank size='md'>
+                <Button type="primary" onClick={this.showName}>点击</Button>
+            <div className={styles.box}>
+            box
+            </div>
+            <Flex>
+            <Flex.Item>111</Flex.Item>
+            <Flex.Item>222</Flex.Item>
+            <Flex.Item>111</Flex.Item>
+            <Flex.Item>222</Flex.Item>
+            </Flex>
+                </WingBlank>
+                 <h1>这是首页</h1>
+            </React.Fragment>
+
+
+    );
+  }
 }
-ReactDOM.render(<MainIndex/>, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app')); //eslint-disable-line
