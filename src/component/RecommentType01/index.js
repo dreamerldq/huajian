6d024fa92553d66aec23e11bd6f971c6a9116345
qiuqiu <qiuqiu@xintiaotime.com>
2018-08-03 18:17:08 +0800

    feat: 修改本周热间样式

--- src/component/RecommentType01/index.js
+++ src/component/RecommentType01/index.js
@@ -14,10 +14,16 @@ export default class Recommend extends React.Component {
             <div className={styles.article_container} key={item.id}>
                 <img className={styles.article_image} src={`https://t3908.xintiaotime.com${item.image}`}></img>
                 <div className={styles.article_texts}>
-                    <div>{item.title}</div>
-                    <div className={styles.title}>{item.desc}</div>
-                    <div>作者：{item.zuozhe}</div>
-                    <div className={styles.article_type}>{item.tstype}</div>
+                    <div className={styles.title}>{item.title}</div>
+                    <div className={styles.des}>{item.desc}</div>
+                    <div className={styles.bottomIntro}>
+                        <div className={styles.author}>作者：{item.zuozhe}</div>
+                        <div className={styles.typeGroup}>
+                            <span>{item.xstype}</span>
+                            <span>{item.tstype}</span>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
