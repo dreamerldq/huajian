d0b190273099028bb0cfaec11fc0abb41d5b0132
qiuqiu <qiuqiu@xintiaotime.com>
2018-11-01 19:46:00 +0800

    自定义组件table添加loading

--- src/Component/HuaTable/index.jsx
+++ src/Component/HuaTable/index.jsx
@@ -7,7 +7,7 @@ export default class HuaTable extends React.Component{
   
   }
   render(){
-    const {columns, dataSource,bordered, local} = this.props
+    const {columns, dataSource,bordered, local, loading} = this.props
     const columnsDataIndex = columns.map((column) => {
       return column.dataIndex
     })
@@ -17,33 +17,46 @@ export default class HuaTable extends React.Component{
       <thead>
           <tr className={styles.table_tr}>
             {columns.map((column, index) => {
-              return <th className={ bordered ? styles.table_th_solid :styles.table_th}  key={column.key || index}>{column.title}</th>
+              return <th width={column.width} className={ bordered ? styles.table_th_solid :styles.table_th}  key={column.key || index}>{column.title}</th>
             })}
           </tr>
         </thead>
-        <tbody>
-          {
-            dataSource.length>0 ?
-            dataSource.map((data,index) => { // dataSource 数据源组  data单个数据源 
-              return(
-                <tr className={styles.table_tr} key={index}>
-                  {
-                   Object.keys(data).map((item, index_key) => { // item单个数据源对象的key  index 第几个key
-                   const column_idx =  _.indexOf(columnsDataIndex, item)
-                   if(column_idx !== -1){
-                    const column = columns[column_idx]
-                    return (column).render ?
-                     <td className={bordered ? styles.table_td_solid: styles.table_td} style={{width: `${column.width || 50}px`}} key={`${item}_${index_key}`}>{column.render(data[item], data, index)}</td> :
-                     <td className={bordered ? styles.table_td_solid: styles.table_td} style={{width: `${column.width || 50}px`}} key={`${item}_${index_key}`}>{data[item]}</td>
-                   }  
-                   })
-                  }
-                </tr>
-              )
-            }):
-            <tr><td>{local.emptyText}</td></tr>
-          }
-        </tbody>
+       {loading ? 
+      
+         <tbody>
+           <tr>
+            <td>
+              <div className={styles.loading}>
+                加载中.....
+              </div>
+            </td>
+           </tr>
+         </tbody>
+        
+       :
+       <tbody>
+       {
+         dataSource.length>0 ?
+         dataSource.map((data,index) => { // dataSource 数据源组  data单个数据源 
+           return(
+             <tr className={styles.table_tr} key={index}>
+               {
+                Object.keys(data).map((item, index_key) => { // item单个数据源对象的key  index 第几个key
+                const column_idx =  _.indexOf(columnsDataIndex, item)
+                if(column_idx !== -1){
+                 const column = columns[column_idx]
+                 return (column).render ?
+                  <td className={bordered ? styles.table_td_solid: styles.table_td} style={{width: `${column.width || 50}px`}} key={`${item}_${index_key}`}>{column.render(data[item], data, index)}</td> :
+                  <td className={bordered ? styles.table_td_solid: styles.table_td} style={{width: `${column.width || 50}px`}} key={`${item}_${index_key}`}>{data[item]}</td>
+                }  
+                })
+               }
+             </tr>
+           )
+         }):
+         <tr><td>{local.emptyText}</td></tr>
+       }
+     </tbody>}
       </table>
        
       </div>
--- src/Route/HuaComponents/index.jsx
+++ src/Route/HuaComponents/index.jsx
@@ -170,6 +170,7 @@ export default class HuaComponents extends React.Component{
               dataSource={dataSource}
               columns={columns}
               bordered
+              loading
               local={{
                 emptyText: '暂无数据'
               }}
@@ -196,28 +197,28 @@ export default class HuaComponents extends React.Component{
             </HuaTabs>
 
             <HuaCollapse activeKey={this.state.activeKey} onChange={this.handlePanel}>
-               <HuaCollapse.Panel header="This is panel  header 1" value="1">
+               <HuaCollapse.Panel key="1" header="This is panel  header 1" value="1">
                   <p>面板一,面板一,面板一,面板一,面板一,面板一</p>
                </HuaCollapse.Panel>
 
-               <HuaCollapse.Panel header="This is panel  header 2" value="2">
+               <HuaCollapse.Panel key="2" header="This is panel  header 2" value="2">
                <p>面板二,面板二,面板二,面板二,面板二,面板二</p>
                </HuaCollapse.Panel>
 
-               <HuaCollapse.Panel header="This is panel  header 3" value="3">
+               <HuaCollapse.Panel key="3" header="This is panel  header 3" value="3">
                <p>面板三,面板三,面板三,面板三,面板三,面板三</p>
                </HuaCollapse.Panel>
             </HuaCollapse>
              <HuaTimeLine>
-                <HuaTimeLine.Item color="green">
+                <HuaTimeLine.Item key="1" color="green">
                   <p>Create a services site 2015-09-01</p>
                   <p>Create a services site 2015-09-01</p>
                   <p>Create a services site 2015-09-01</p>
                   <p>Create a services site 2015-09-01</p>
                   </HuaTimeLine.Item>
-                <HuaTimeLine.Item>Solve initial network problems 2015-09-01</HuaTimeLine.Item>
-                <HuaTimeLine.Item>Technical testing 2015-09-01</HuaTimeLine.Item>
-                <HuaTimeLine.Item>Network problems being solved 2015-09-01</HuaTimeLine.Item>
+                <HuaTimeLine.Item key="2">Solve initial network problems 2015-09-01</HuaTimeLine.Item>
+                <HuaTimeLine.Item key="3">Technical testing 2015-09-01</HuaTimeLine.Item>
+                <HuaTimeLine.Item key="4">Network problems being solved 2015-09-01</HuaTimeLine.Item>
              </HuaTimeLine>
              <Button onClick={this.notice.bind(this, 1)}>按钮1</Button>
              <Button onClick={this.notice.bind(this, 2)}>按钮2</Button>
